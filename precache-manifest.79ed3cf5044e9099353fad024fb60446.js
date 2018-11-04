6930d756cd5fbc720d68f58fa8d714c628e7a2b5
Ahmed-Aljasim <ahmed1700@windowslive.com>
2018-11-04 06:22:52 +0300

    Updates

---
+++ precache-manifest.79ed3cf5044e9099353fad024fb60446.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "084ac45aee39707f976e",
+    "url": "/cv/static/js/runtime~main.084ac45a.js"
+  },
+  {
+    "revision": "67e1e2f5e29e5c73d7ad",
+    "url": "/cv/static/js/main.67e1e2f5.chunk.js"
+  },
+  {
+    "revision": "b0e90f2ec75a7e525bdb",
+    "url": "/cv/static/js/1.b0e90f2e.chunk.js"
+  },
+  {
+    "revision": "67e1e2f5e29e5c73d7ad",
+    "url": "/cv/static/css/main.91443aef.chunk.css"
+  },
+  {
+    "revision": "4ea774b53a643c1d2d08b47c0c1d3c91",
+    "url": "/cv/index.html"
+  }
+];
